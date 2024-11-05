a46eb3f7f5d3778b54aeaace171db10a384ab46b
Andrew Nkole <andynkole@gmail.com>
2024-11-05 22:59:55 +0300

    Add files via upload

---
+++ 1st task.py
@@ -0,0 +1,16 @@
+from csv import reader
+
+count = 0  
+
+with open('books-en.csv', 'r') as csvfile:
+    table = reader(csvfile, delimiter=';')
+    for row in table:
+        if len(row[1]) > 30:
+            try:
+                price = float(row[6].replace(',', '.'))
+                if price >= 200:
+                    count += 1
+            except ValueError:
+                print(f'Invalid price: {row[6]}')
+
+print('Total number is: ', count)
